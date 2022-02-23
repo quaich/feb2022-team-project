f5f17aa8e100ba00d0e5122dd1f51f208aec78e3
quaich <mail@declant.com>
2022-02-23 13:26:31 +0000

    initial commit

---
+++ online-antique-storage-postgres-app/database.sql
@@ -0,0 +1,79 @@
+CREATE TABLE catagories(
+	catagoryId SERIAL,
+	catagoryName VARCHAR(50) NOT NULL,
+	catagoryDescription VARCHAR(50) DEFAULT 'No Description Available',
+	catagoryImage VARCHAR(50) DEFAULT 'assets/defaultCatagory.jpg',
+
+	PRIMARY KEY (catagoryId)
+);
+
+CREATE TABLE subCatagories(
+	subCatagoryId SERIAL,
+	subCatagoryName VARCHAR(50) NOT NULL,
+	subCatagoryDescription VARCHAR(50) DEFAULT 'No Description Available',
+	subCatagoryImage VARCHAR(50) DEFAULT 'assets/defaultCatagory.jpg',
+
+	catagoryId int NOT NULL,
+	
+	PRIMARY KEY(subCatagoryId),
+	FOREIGN KEY(catagoryId) REFERENCES catagories(catagoryId)
+);
+
+CREATE TABLE product(
+	productId SERIAL,
+	productName VARCHAR(50) NOT NULL,
+	productDescription VARCHAR(100) DEFAULT 'No Description Available.',
+	productImage VARCHAR(50) DEFAULT 'assets/defaultProduct.jpg',
+	subCatagoryId int NOT NULL,
+
+	price float NOT NULL,
+	discountPercent int DEFAULT 0,
+	discountPrice int DEFAULT NULL,
+	
+	reviewScore int DEFAULT 0,
+	stock int DEFAULT 0,
+	
+	PRIMARY KEY (productId),
+	FOREIGN KEY (subCatagoryId) REFERENCES subcatagories(subCatagoryId)
+);
+
+CREATE TABLE users(
+	userId SERIAL,
+	username VARCHAR(15) NOT NULL,
+	passwd VARCHAR (18) NOT NULL, --idealy hashed and salted
+	
+	firstName VARCHAR (10),
+	lastName VARCHAR(10),
+	address VARCHAR(50),
+	contractNo VARCHAR(15),
+	
+	administrator boolean DEFAULT FALSE,
+	
+	PRIMARY KEY (userId)
+);
+
+CREATE TABLE orders(
+	orderId SERIAL,
+	cartItemId int NOT NULL,
+	totalPrice float DEFAULT 0.00,
+	userId int NOT NULL,
+	
+	PRIMARY KEY (orderId),
+	FOREIGN KEY (userId) REFERENCES users(userId)
+);
+
+CREATE TABLE cartItem( -- many to one with order
+	cartItemId SERIAL,
+	orderId int NOT NULL,
+	productId int NOT NULL,
+	quantity int DEFAULT 1,
+	
+	PRIMARY KEY(cartItemId),
+	FOREIGN KEY(productId) REFERENCES product(productId),
+	FOREIGN KEY(orderId) REFERENCES orders(orderId)
+);
+
+
+
+
+
