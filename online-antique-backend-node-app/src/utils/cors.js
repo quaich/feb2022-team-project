056ba59000a43a76ce9ed1c30c81c824e931649b
quaich <mail@declant.com>
2022-02-25 13:13:00 +0000

    Add POST method Login and CORS module

---
+++ online-antique-backend-node-app/src/utils/cors.js
@@ -0,0 +1,14 @@
+// middleaware - to enable CORS at server-side
+
+module.exports = {
+    insCors: function (req, res, next){
+        console.log("within cors configuration middleware");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        next();
+      }
+};
